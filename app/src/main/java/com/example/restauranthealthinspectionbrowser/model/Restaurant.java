6bd27dfe93b4253224c79f8ba5b8b973f38f7b34
Colin Huang <chuang7791@gmail.com>
2020-07-04 06:08:44 -0700

    Add Restaurant class

--- app/src/main/java/com/example/restauranthealthinspectionbrowser/model/Restaurant.java
+++ app/src/main/java/com/example/restauranthealthinspectionbrowser/model/Restaurant.java
@@ -1,4 +1,25 @@
 package com.example.restauranthealthinspectionbrowser.model;
 
 public class Restaurant {
+    private String mID;
+    private String mName;
+    private String mAddress;
+
+    public Restaurant() {
+        mID = "SDFO-8HKP7E";
+        mName = "Pattullo A&W";
+        mAddress = "12808 King George Blvd, Surrey";
+    }
+
+    public String getID() {
+        return mID;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getAddress() {
+        return mAddress;
+    }
 }
