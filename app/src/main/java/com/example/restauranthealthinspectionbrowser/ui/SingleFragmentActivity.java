5c72e0a5e2b8168b0b5f62736cc215afc2e69db7
Colin Huang <chuang7791@gmail.com>
2020-07-04 11:51:15 -0700

    Add SingleFragmentActivity abstract class

---
+++ app/src/main/java/com/example/restauranthealthinspectionbrowser/ui/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.example.restauranthealthinspectionbrowser.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import com.example.restauranthealthinspectionbrowser.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
