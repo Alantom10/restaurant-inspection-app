4c1cf98db892384c740bc279afe12564413796b1
Colin Huang <chuang7791@gmail.com>
2020-07-05 18:13:34 -0700

    Add DateHelper class
    DateHelper class includes static method to format dates for display

--- app/src/main/java/com/example/restauranthealthinspectionbrowser/ui/RestaurantFragment.java
+++ app/src/main/java/com/example/restauranthealthinspectionbrowser/ui/RestaurantFragment.java
@@ -22,7 +22,6 @@ public class RestaurantFragment extends Fragment {
 
     private Restaurant mRestaurant;
 
-    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
