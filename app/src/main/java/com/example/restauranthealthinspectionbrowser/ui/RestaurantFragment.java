bda17aca3c2cf620016e8c416fac504bfc30d96e
c.huang <colin_huang_3@sfu.ca>
2020-07-08 02:34:05 -0700

    Bind inspections to inspection list
    Update layout widgets to display information of each inspection

--- app/src/main/java/com/example/restauranthealthinspectionbrowser/ui/RestaurantFragment.java
+++ app/src/main/java/com/example/restauranthealthinspectionbrowser/ui/RestaurantFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.restauranthealthinspectionbrowser.R;
+import com.example.restauranthealthinspectionbrowser.model.DateHelper;
 import com.example.restauranthealthinspectionbrowser.model.Inspection;
 import com.example.restauranthealthinspectionbrowser.model.InspectionManager;
 import com.example.restauranthealthinspectionbrowser.model.Restaurant;
@@ -29,14 +32,15 @@ public class RestaurantFragment extends Fragment {
     private InspectionAdapter mAdapter;
 
     private Restaurant mRestaurant;
+    private String mRestaurantID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_restaurant, container, false);
 
-        String restaurantID = getActivity().getIntent().getStringExtra(EXTRA_RESTAURANT_ID);
-        mRestaurant = RestaurantManager.getInstance(getActivity()).getRestaurant(restaurantID);
+        mRestaurantID = getActivity().getIntent().getStringExtra(EXTRA_RESTAURANT_ID);
+        mRestaurant = RestaurantManager.getInstance(getActivity()).getRestaurant(mRestaurantID);
 
         mTitleTextView = (TextView) view.findViewById(R.id.title);
         mAddressTextView = (TextView) view.findViewById(R.id.address);
@@ -59,14 +63,49 @@ public class RestaurantFragment extends Fragment {
 
     private void updateRecyclerView() {
         InspectionManager manager = InspectionManager.getInstance(getActivity());
-        List<Inspection> inspections = manager.getInspections();
+        List<Inspection> inspections = manager.getInspectionsForRestaurant(mRestaurantID);
         mAdapter = new InspectionAdapter(inspections);
         mInspectionRecyclerView.setAdapter(mAdapter);
     }
 
     private class InspectionHolder extends RecyclerView.ViewHolder {
+        private TextView mNumCriticalIssuesTextView;
+        private TextView mNumNonCriticalIssuesTextView;
+        private TextView mHazardLevelTextView;
+        private TextView mDateTextView;
+        private ImageView mHazardLevelImageView;
+
+        private Inspection mInspection;
+
         public InspectionHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_inspection, parent, false));
+
+            mNumCriticalIssuesTextView = (TextView) itemView.findViewById(R.id.num_critical_issues);
+            mNumNonCriticalIssuesTextView = (TextView) itemView.findViewById(R.id.num_non_critical_issues);
+            mHazardLevelTextView = (TextView) itemView.findViewById(R.id.hazard_level);
+            mDateTextView = (TextView) itemView.findViewById(R.id.inspection_date);
+            mHazardLevelImageView = (ImageView) itemView.findViewById(R.id.hazard_level_icon);
+        }
+
+        public void bind(Inspection inspection) {
+            mInspection = inspection;
+
+            mNumCriticalIssuesTextView.setText(getString(R.string.num_critical_issues, mInspection.getNumOfCritical()));
+            mNumNonCriticalIssuesTextView.setText(getString(R.string.num_non_critical_issues, mInspection.getNumOfNonCritical()));
+            mDateTextView.setText(DateHelper.getDisplayDate(mInspection.getInspectionDate()));
+
+            String hazardLevel = inspection.getHazardRating();
+            mHazardLevelTextView.setText(getString(R.string.hazard_level, hazardLevel));
+            if (hazardLevel.equals("High")) {
+                mHazardLevelTextView.setTextColor(ContextCompat.getColor(getActivity(), R.color.highHazardLevel));
+                mHazardLevelImageView.setImageResource(R.drawable.ic_high_level_black_24dp);
+            } else if (hazardLevel.equals("Moderate")) {
+                mHazardLevelTextView.setTextColor(ContextCompat.getColor(getActivity(), R.color.moderateHazardLevel));
+                mHazardLevelImageView.setImageResource(R.drawable.ic_moderate_level_black_24dp);
+            } else {
+                mHazardLevelTextView.setTextColor(ContextCompat.getColor(getActivity(), R.color.lowHazardLevel));
+                mHazardLevelImageView.setImageResource(R.drawable.ic_low_level_black_24dp);
+            }
         }
     }
 
@@ -83,6 +122,8 @@ public class RestaurantFragment extends Fragment {
         }
         @Override
         public void onBindViewHolder(InspectionHolder holder, int position) {
+            Inspection inspection = mInspections.get(position);
+            holder.bind(inspection);
         }
         @Override
         public int getItemCount() {
